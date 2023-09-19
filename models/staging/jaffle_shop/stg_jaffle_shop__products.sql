58a21d24afe3c85a73dd5ce39a8d9c66c0cd4b0a
Lasse Benninga <devops.pipeline@example.com>
2023-09-19 23:42:36 +0200

    updat

--- models/staging/jaffle_shop/stg_jaffle_shop__products.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__products.sql
@@ -2,7 +2,7 @@ with
     raw_source as (select * from {{ source("jaffle_shop", "products") }}),
 
     final as (
-        select sku, name, type, price, description 
+        select sku, name, type, price, description
         from raw_source
         )
 
